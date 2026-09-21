4bd2311fb4db567c37295031d6528434c675d782
Paphawadee-Ch_tcct <Paphawadee.Ch@tcc-technology.com>
2026-09-21 14:04:53 +0700

    Complete initial Python exercises

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Paphawadee"
+last_name = "Chansorn"
+
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Paphawadee"
+last_name = "Chansorn"
+whole_name = f"{first_name} {last_name}"
+
+print(whole_name)
