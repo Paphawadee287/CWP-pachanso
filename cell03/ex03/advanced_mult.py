5032496493ad3bcf2e17277df9e747f144097310
Paphawadee287 <66070287@kmitl.ac.th>
2026-09-21 19:41:59 +0700

    add advanced_mult

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+import sys
+
+if len(sys.argv) != 1:
+    print("none")
+else:
+    i = 0
+    while i < 11:
+        print(f"Table de {i}:", end="")
+
+        j = 0
+        while j < 11:
+            print(f" {i * j}", end="")
+            j += 1
+
+        print()
+        i += 1
